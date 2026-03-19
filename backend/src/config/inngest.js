8e84f14c5d10874e0a87d9155ec6e09dd1cbbbf3
Lawrence <code.matt.eagles@gmail.com>
2026-03-19 16:40:04 +0100

    fixed inngest node.js function email bug

--- backend/src/config/inngest.js
+++ backend/src/config/inngest.js
@@ -25,7 +25,7 @@ const syncUser = inngest.createFunction(
 
     const newUser = {
       clerkId: id,
-      email: email_addresses[0]?.email_addresses,
+      email,
       name: `${first_name || ""} ${last_name || ""}` || "User",
       imageUrl: image_url,
       addresses: [],
