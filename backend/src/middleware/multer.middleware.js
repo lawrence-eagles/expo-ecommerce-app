d23f8847b9fe6232b3454d2a79b4ca1bf3dc7909
Lawrence <code.matt.eagles@gmail.com>
2026-03-20 18:12:36 +0100

    bug fixes in multer middleware

--- backend/src/middleware/multer.middleware.js
+++ backend/src/middleware/multer.middleware.js
@@ -16,12 +16,12 @@ const fileFilter = (req, file, cb) => {
   const extname = allowedTypes.test(
     path.extname(file.originalname).toLocaleLowerCase(),
   );
-  const mimeTypes = allowedTypes.test(file.mimeTypes);
+  const mimeTypes = allowedTypes.test(file.mimetype);
 
   if (extname && mimeTypes) {
     cb(null, true);
   } else {
-    cb(new Error("Only image files are allowed (jpeg, jpg, png, webpg)"));
+    cb(new Error("Only image files are allowed (jpeg, jpg, png, webp)"));
   }
 };
 
