01ea47ba33de99eaf2e4125d9b9e821fdceaa1fb
Lawrence <code.matt.eagles@gmail.com>
2026-03-24 22:24:59 +0100

    re-implemented the auth middleware to use getAuth instead of requireAuth

--- backend/src/middleware/auth.middleware.js
+++ backend/src/middleware/auth.middleware.js
@@ -1,13 +1,15 @@
-import { requireAuth } from "@clerk/express";
+import { requireAuth, getAuth } from "@clerk/express";
 import { User } from "../models/user.model.js";
 import { ENV } from "../config/env.js";
 
 export const protectRoute = [
-  requireAuth(),
+  // requireAuth(),
   async (req, res, next) => {
     try {
-      const clerkId = req.auth().userId;
-      if (!clerkId)
+      const { userId: clerkId, isAuthenticated } = getAuth(req);
+      // const clerkId = req.auth().userId;
+
+      if (!clerkId || !isAuthenticated)
         return res
           .status(401)
           .json({ message: "Unauthorized - invalid token" });
@@ -16,6 +18,7 @@ export const protectRoute = [
       if (!user) return res.status(404).json({ message: "User not found" });
 
       req.user = user;
+
       next();
     } catch (error) {
       console.error("Error in protectRoute middleware", error);
