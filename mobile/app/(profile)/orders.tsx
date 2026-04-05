b39b873f20a0ce5af49d913dc606ccf48d7cb8ca
Lawrence <code.matt.eagles@gmail.com>
2026-04-05 23:47:22 +0100

    fix bug and update seeder file

--- mobile/app/(profile)/orders.tsx
+++ mobile/app/(profile)/orders.tsx
@@ -235,12 +235,12 @@ function LoadingUI() {
 
 function ErrorUI() {
   return (
-    <View className="flex-1 items center justify-center px-6">
+    <View className="flex-1 items-center justify-center px-6">
       <Ionicons name="alert-circle-outline" size={64} color={"#FF6B6B"} />
       <Text className="text-text-primary font-semibold text-xl mt-4">
         Failed to load orders
       </Text>
-      <Text className="text-text-secondary text-center mt">
+      <Text className="text-text-secondary text-center mt-2">
         Please check your connection and try again
       </Text>
     </View>
